3fa23a334f71bb7e9698b24dff5976b93b6438c2
Saikiran78-hub <saikiranofficial9@gmail.com>
2025-07-23 00:10:31 +0530

    First commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='smartformatter',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=['inflect'],
+    author='saikiran',
+    description='Utility functions for smart formatting of names, phones, numbers.',
+)
